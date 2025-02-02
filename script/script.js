6cd9c80479bbbdd8de6b3b3a01c21344943747ae
Cezar Viana <cezar.viana@gmail.com>
2025-02-02 16:43:22 -0300

    <challenges>: desafios para depois de terminar a parte do curso de JS

--- script/script.js
+++ script/script.js
@@ -1,35 +1,39 @@
-const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-playlists')
+/* 
+A response está trazendo somente 1 elemento, 
+TENTAR trazer todos contendo o searchTerm;
+*/
+const resultArtist = document.getElementById("result-artist");
+const playlistContainer = document.getElementById("result-playlists");
+const searchInput = document.getElementById("search-input");
 
-function resquestApi(searchTerm){
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
+function requestApi(searchTerm) {
+  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+    .then((response) => response.json())
+    .then((results) => displayResults(results));
 }
 
-function displayResults(result) {
-    resultPlaylist.classList.add('hidden');
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+function displayResults(results) {
+  hidePlaylists();
+  const artistImage = document.getElementById("artist-img");
+  const artistName = document.getElementById("artist-name");
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
-    });
-
-    resultArtist.classList.remove('hidden');
+  results.forEach((element) => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
+  });
+  resultArtist.classList.remove("hidden");
 }
 
-document.addEventListener('input', function(){
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm ===''){
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return;
-    }
+function hidePlaylists() {
+  playlistContainer.classList.add("hidden");
+}
 
-    resquestApi(searchTerm);
-
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultArtist.classList.add("hidden");
+    playlistContainer.classList.remove("hidden");
+    return;
+  }
+  requestApi(searchTerm);
 });
